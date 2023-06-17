79fca90b9d1f775a9e82077106e97d0638cc6c88
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-06-17 04:03:07 -0500

    add better comments in config

--- src/main/java/net/pl3x/rainglow/configuration/Config.java
+++ src/main/java/net/pl3x/rainglow/configuration/Config.java
@@ -8,10 +8,16 @@ import org.jetbrains.annotations.Nullable;
 public class Config extends AbstractConfig {
     @Key("settings.color-mode")
     @Comment("""
-            Color mode to use.""")
+            Color mode sets which colors are allowed to be used.
+            Modes are: all_colours, aro_pride, ace_pride, bi_pride, gay_pride,
+            genderfluid_pride, lesbian_pride, monochrome, enby_pride, pan_pride,
+            rainbow, trans_pride, and vanilla.""")
     public static Color.Mode COLOR_MODE = Color.Mode.ALL_COLOURS;
 
     @Key("messages.color-not-allowed")
+    @Comment("""
+            The message to display to users trying to use dye on a mob that
+            isn't allowed by the current color mode. (set to "" to disable)""")
     public static String COLOR_NOT_ALLOWED = "<red>That color is not allowed!";
 
     private static final Config CONFIG = new Config();
